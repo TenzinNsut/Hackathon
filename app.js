9707903392c4242a4bd302bbf62feb89b23c39c5
TenzinNsut <tenzin.lekshey.ug21@nsut.ac.in>
2022-11-12 12:27:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static folder
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log(`server is listening on ${port}`);
+});
